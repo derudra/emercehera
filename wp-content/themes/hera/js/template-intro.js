20971e29554f13d829d566c2f5da8e565ed50ca3
TECH Mentor <92755913+derudra@users.noreply.github.com>
2022-03-10 09:49:35 +0530

    Commit123

---
+++ wp-content/themes/hera/js/template-intro.js
@@ -0,0 +1,31 @@
+(function($) { "use strict";
+	$(document).ready(function () {
+	    $('.flippy').flippy({
+	        interval: 4,
+	        speed: 500,
+	        stop: false,
+	        distance: "100px"
+	    });
+	});					
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
